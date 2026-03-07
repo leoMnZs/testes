bd04e6da05c69be5509bb3c851ffc201f4c6440e
Leonardo <leonardoffbs381@gmail.com>
2026-03-07 14:34:37 -0300

    CSS e JS externos

---
+++ script.js
@@ -0,0 +1,12 @@
+function mudarCor() {
+    document.body.style.backgroundColor = "blue"
+        }
+         function mudarCor2() {
+    document.body.style.backgroundColor = "red"
+        }
+        function mudarCor3() {
+    document.body.style.backgroundColor = "white"
+        }
+         function mudarCor4() {
+    document.body.style.backgroundColor = "black"
+        }
