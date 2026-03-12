d9859592fa7306b6397f884e2d7fad7739b3aca8
Leonardo <leonardoffbs381@gmail.com>
2026-03-12 16:01:48 -0300

    js

---
+++ script.js
@@ -0,0 +1,6 @@
+var num1 = parseFloat(prompt("digite o primeiro numero"));//cria variavel e a janela para colocar a variavel
+var num2 = parseFloat(prompt("digite o segundo numero"));
+
+var resultado = num1 + num2;//cria variavel do resultado
+console.log("a soma de " + num1 + " e " + num2 + " é de = " + resultado)//mostra o resultado no console (sempre colocar "+" antes e depois de cada varial, salvo na ultima)
+alert("o seu resultado é = " + resultado)//mostra o resultado
